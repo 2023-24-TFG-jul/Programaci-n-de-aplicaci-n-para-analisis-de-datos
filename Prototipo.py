b1207798fe5c90fd01e2ba10a6bc5062eace24d6
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-01-29 18:05:16 +0100

    Connectividad con la base de datos de posgres

--- Prototipo.py
+++ Prototipo.py
@@ -3,3 +3,6 @@
 #Fecha:25/01/24
 #Versión:0.001
 #Descripción: Por ahora nada
+import psycopg2
+
+conn=psycopg2.connect(host="localhost",dbname="postgres", user="postgres", password="1234",port=5432)
