45056bc7fee2bd31491533b05e6ef2f1fbed9e31
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-01-30 23:42:57 +0100

    Comentarios

--- Prototipo.py
+++ Prototipo.py
@@ -3,14 +3,19 @@
 #Fecha:25/01/24
 #Versión:0.001
 #Descripción: Por ahora nada
+#########################################################################################################################
+#Definimos los imports
 import psycopg2
+
+##########################################################################################################################
+#Parametros de la base de datos
+    
 
 conn=psycopg2.connect(host="localhost",dbname="postgres", user="postgres", password="1234",port=5432)
 
 cur=conn.cursor()
 operacion="""CREATE TABLE IF NOT EXISTS person (id INT PRIMARY KEY,name VARCHAR (255),age INT, gender CHAR ) """
 cur.execute(operacion)
-# #Hacer cosas
 conn.commit()
 
 cur.close()
