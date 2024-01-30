eae5631c1b8b5ed93b9d81843afe808a33e77332
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-01-30 20:56:30 +0100

    Conexión Python y postgres

--- Prototipo.py
+++ Prototipo.py
@@ -6,3 +6,12 @@
 import psycopg2
 
 conn=psycopg2.connect(host="localhost",dbname="postgres", user="postgres", password="1234",port=5432)
+
+cur=conn.cursor()
+operacion="""CREATE TABLE IF NOT EXISTS person (id INT PRIMARY KEY,name VARCHAR (255),age INT, gender CHAR ) """
+cur.execute(operacion)
+# #Hacer cosas
+conn.commit()
+
+cur.close()
+conn.close()
