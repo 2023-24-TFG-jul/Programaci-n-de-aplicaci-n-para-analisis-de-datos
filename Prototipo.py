b45e76b555920e619fb2f1d16ad93d45f51d9fe6
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-01-30 23:52:30 +0100

    Más comentarios

--- Prototipo.py
+++ Prototipo.py
@@ -14,13 +14,16 @@ dataname="postgres"
 datauser="postgres"
 datapass="1234"
 dataport=5432
-
+#Establecemos la conexion con la base de datos
 conn=psycopg2.connect(host=datahost,dbname=dataname, user=datauser, password=datapass,port=dataport)
-
+#Inicializamos el cursor con el que operaremos en la base de datos
 cur=conn.cursor()
+#Guardamos una operación en forma de string 
 operacion="""CREATE TABLE IF NOT EXISTS person (id INT PRIMARY KEY,name VARCHAR (255),age INT, gender CHAR ) """
+#Enviamos la operación a la base de datos
 cur.execute(operacion)
 conn.commit()
 
+#Cerramos el cursor que vamos a utilizar y la conexión para que no nos de errores cuando los queramos volver a usar
 cur.close()
 conn.close()
