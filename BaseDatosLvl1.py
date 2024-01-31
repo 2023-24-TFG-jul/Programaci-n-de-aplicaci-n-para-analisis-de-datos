91b057bb692e7892c7d6769f217bcd4666bca2ca
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-01-31 22:38:52 +0100

    Definimos el constructor de la clase

--- BaseDatosLvl1.py
+++ BaseDatosLvl1.py
@@ -1,7 +1,7 @@
 #Nombre:BaseDatosLvl1
 #Autor:Álvaro Villar Val
 #Fecha:25/01/24
-#Versión:0.003
+#Versión:0.1
 #Descripción: Por ahora nada
 #########################################################################################################################
 #Definimos los imports
@@ -9,5 +9,16 @@ import psycopg2
 import pandas as pd
 
 #Inicializamos la Clase de creación de base de datos
-def BaseDatosLlv1():
-    
+class BaseDatosLlv1:
+    #Definimos el constructor de la base de datos que hara la conexion con la base de datos
+    def __init__(self):
+        #Parametros de la base de datos
+        self.datahost="localhost"
+        self.dataname="postgres"
+        self.datauser="postgres"
+        self.datapass="1234"
+        self.dataport=5432
+        #Establecemos la conexion con la base de datos
+        self.conn=psycopg2.connect(host=self.datahost,dbname=self.dataname, user=self.datauser, password=self.datapass,port=self.dataport)
+        #Inicializamos el cursor con el que operaremos en la base de datos
+        self.cur=self.conn.cursor()
