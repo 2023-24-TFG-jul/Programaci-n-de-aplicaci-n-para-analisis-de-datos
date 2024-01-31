f7ead116ef7f1ee232f5777e12d672dc5b831aae
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-01-31 22:12:35 +0100

    Creamos el programa que se ocupara de gestionar la base de datos de primer nivel

---
+++ BaseDatosLvl1.py
@@ -0,0 +1,13 @@
+#Nombre:BaseDatosLvl1
+#Autor:Álvaro Villar Val
+#Fecha:25/01/24
+#Versión:0.003
+#Descripción: Por ahora nada
+#########################################################################################################################
+#Definimos los imports
+import psycopg2
+import pandas as pd
+
+#Inicializamos la Clase de creación de base de datos
+def BaseDatosLlv1():
+    
