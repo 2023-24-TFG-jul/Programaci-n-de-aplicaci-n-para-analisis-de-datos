7a747858f491b4c9643d831f34253d2a65035c98
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-01-31 22:52:43 +0100

    Version 0.15: Añadido Corte de comunicación, Actualizar y obtener datos de la base

--- BaseDatosLvl1.py
+++ BaseDatosLvl1.py
@@ -1,7 +1,7 @@
 #Nombre:BaseDatosLvl1
 #Autor:Álvaro Villar Val
 #Fecha:25/01/24
-#Versión:0.1
+#Versión:0.15
 #Descripción: Por ahora nada
 #########################################################################################################################
 #Definimos los imports
@@ -10,7 +10,9 @@ import pandas as pd
 
 #Inicializamos la Clase de creación de base de datos
 class BaseDatosLlv1:
+
     #Definimos el constructor de la base de datos que hara la conexion con la base de datos
+    ####################################################################################################################
     def __init__(self):
         #Parametros de la base de datos
         self.datahost="localhost"
@@ -22,3 +24,27 @@ class BaseDatosLlv1:
         self.conn=psycopg2.connect(host=self.datahost,dbname=self.dataname, user=self.datauser, password=self.datapass,port=self.dataport)
         #Inicializamos el cursor con el que operaremos en la base de datos
         self.cur=self.conn.cursor()
+    ########################################################################################################################
+        
+    #Obtenemos los datos de una fecha a otra fecha
+    ####################################################################################################################
+    def obtenerdat(self,ini,fin):
+        orden=""" """
+        #Enviamos la operación a la base de datos
+        self.cur.execute(orden)
+        self.conn.commit()
+    ######################################################################################################################
+
+    #Actualizamos la base de datos con los ultimos datos que hayamos obtenido
+    ##########################################################################################################################   
+    def actualizar(self):
+        orden=""" """
+        #Enviamos la operación a la base de datos
+        self.cur.execute(orden)
+        self.conn.commit()
+    #Definimos el Cierre de la conexión con la base de datos
+    def stop(self):
+        #Cerramos el cursor que vamos a utilizar y la conexión para que no nos de errores cuando los queramos volver a usar
+        self.cur.close()
+        self.conn.close() 
+
