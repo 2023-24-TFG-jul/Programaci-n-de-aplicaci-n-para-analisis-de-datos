7aac329998e0618c0aa01d06a912c6dea3401615
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-02-02 23:01:06 +0100

    Más Prubeas, recursividad en la base de datos

--- Prototipo.py
+++ Prototipo.py
@@ -18,6 +18,10 @@ dataport=5432
 conn=psycopg2.connect(host=datahost,dbname=dataname, user=datauser, password=datapass,port=dataport)
 #Inicializamos el cursor con el que operaremos en la base de datos
 cur=conn.cursor()
+operacion="""DROP TABLE IF EXISTS person"""
+#Enviamos la operación a la base de datos
+cur.execute(operacion)
+conn.commit()
 #Guardamos una operación en forma de string 
 operacion="""CREATE TABLE IF NOT EXISTS person (id INT PRIMARY KEY,name VARCHAR (255),age INT, gender CHAR ) """
 #Enviamos la operación a la base de datos
@@ -28,11 +32,20 @@ operacion="""SELECT * FROM person """
 cur.execute(operacion)
 conn.commit()
 print(cur.fetchall())
+name=['Hugo','Martín','Lucas','Mateo','Leo' ,'Daniel','Alejandro','Pablo','Manuel','Álvaro','Adrián','David','Mario','Enzo','Diego' ]
+for i in range(15):
+    operacion="""INSERT INTO person (id,name,age,gender) Values (%s,%s,%s,%s) """
+    gender='M'
+    if ((i%2)!= 0):
+        gender='F'
+    value_insert=(i, name[i],i*5,gender)
+    #Enviamos la operación a la base de datos
+    cur.execute(operacion,value_insert)
+    conn.commit()
 operacion="""SELECT * FROM person """
 #Enviamos la operación a la base de datos
 cur.execute(operacion)
 conn.commit()
-print(cur.fetchall()[0][1])
 
 
 
