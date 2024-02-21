c3c9cda5288425dd951b048353545f75e85bb119
Álvaro <alvafrankvvmm@gmail.com>
2024-02-21 19:26:24 +0100

    Pruebas

--- Prototipo.py
+++ Prototipo.py
@@ -11,10 +11,22 @@ from BaseDatosLvl1 import BaseDatosLvl1
 from psycopg2 import sql
 ##########################################################################################################################
 #Parametros de la base de datos
-db1=BaseDatosLvl1()
-db1.injectarimg("Datos\Fotos\\201D_CAM1_20231005_085100_02.jpg","Datos\Fotos\\1.jpg")
-print(db1.obtenerdat("*","images",None,None))
-db1.obtenerImg(240220)
+# db1=BaseDatosLvl1()
+# db1.injectarimg("Datos\Fotos\\201D_CAM1_20231005_085100_02.jpg","Datos\Fotos\\1.jpg")
+# print(db1.obtenerdat("*","images",None,None))
+# db1.obtenerImg(240220)
+with open("Datos\Fotos\\201D_CAM1_20231005_085100_02.jpg", 'rb') as f:
+            image1_data = f.read()
+with open("Datos\Fotos\\1.jpg", 'rb') as f:
+            image2_data = f.read()
+img1=psycopg2.Binary(image1_data)
+img2=psycopg2.Binary(image2_data)
+print(img1[0])
+print(img2[0])
+with open("Fotos resulta\\foto1.jpg", 'wb') as file:
+                file.write(img1[0])
+with open("Fotos resulta\\foto2.jpg", 'wb') as file:
+                file.write(img2[0])
 # db1.actualizardatos()
 # datos1=db1.obtenerdat("*","radio","23-10-01","23-10-01")
 # datos2=db1.obtenerdat("*","skyscanner","23-12-01","23-12-02")
@@ -22,5 +34,5 @@ db1.obtenerImg(240220)
 # print(datos1)
 # print(datos2)
 # print(datos3)
-db1.stop()
+#db1.stop()
 
