6264181d672a781ec0c70ee2dfc36311664fec8f
Álvaro <alvafrankvvmm@gmail.com>
2024-03-05 23:31:00 +0100

    Log Ver: 0.0.1 Creada una clase log para que gestione el log de la aplicación de manera facil y sencilla

---
+++ Log.py
@@ -0,0 +1,15 @@
+#Nombre:Log
+#Autor:Álvaro Villar Val
+#Fecha:05/03/24
+#Versión:0.0.1
+#Descripción: Creamos una clase para que gestione el log de la aplicación
+#########################################################################################################################
+class Log:
+    def __init__(self):
+
+        self.fichero="log.txt"
+
+    def injeErr(self,error):
+
+    def limpiarLog(self):
+        
