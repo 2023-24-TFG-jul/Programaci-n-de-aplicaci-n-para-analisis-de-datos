d4e65fa718acaae87ad9d3553f2fbe4ac0715e02
Álvaro <alvafrankvvmm@gmail.com>
2024-03-07 22:57:34 +0100

    BD1 Ver:1.0.3 Saneamiento del codigo

--- BaseDatosLvl1.py
+++ BaseDatosLvl1.py
@@ -1,7 +1,7 @@
 #Nombre:BasedatosLvl1
 #Autor:Álvaro Villar Val
 #Fecha:25/01/24
-#Versión:1.0.2
+#Versión:1.0.3
 #Descripción: Base de datos de primer nivel de una central meteorologica de la Universidad de burgos
 #########################################################################################################################
 #Definimos los imports
@@ -18,16 +18,16 @@ class BaseDatosLvl1:
 
 #######################################################################################################################################################################################################################
 #Zona de Creación de la base de datos
-    
+
     #Definimos el constructor de la base de datos que hara la conexion con la base de sectos
     ####################################################################################################################
     def __init__(self):
         #Parametros de la base de datos
-        self.dirradio="Datos\datalogger" #path donde se meteran los archivos que se quieran meter en la base de datos radio
-        self.dircamera="Datos\sky-camera"#path donde se meteran los archivos que se quieran meter en la base de datos skycamera
-        self.dirscanner="Datos\Sky-scanner 2023_12"#path donde se meteran los archivos que se quieran meter en la base de datos skyscanner
-        self.dirimgCam1="Datos\Fotos\CAM1\imagenes"#path en el que se meteran las imagenes de la camara1 que se quiera introducir en la base de datos
-        self.dirimgCam2="Datos\Fotos\CAM2\imagenes"#path en el que se meteran las imagenes de la camara2 que se quieran introducir en la base de datos
+        self.dirradio="Datos\\datalogger" #path donde se meteran los archivos que se quieran meter en la base de datos radio
+        self.dircamera="Datos\\sky-camera"#path donde se meteran los archivos que se quieran meter en la base de datos skycamera
+        self.dirscanner="Datos\\Sky-scanner 2023_12"#path donde se meteran los archivos que se quieran meter en la base de datos skyscanner
+        self.dirimgCam1="Datos\\Fotos\CAM1\imagenes"#path en el que se meteran las imagenes de la camara1 que se quiera introducir en la base de datos
+        self.dirimgCam2="Datos\\Fotos\CAM2\imagenes"#path en el que se meteran las imagenes de la camara2 que se quieran introducir en la base de datos
         self.datahost="localhost" #Host de la base de datos
         self.dataname="postgres"  #Nombre de la base de datos
         self.datauser="postgres"  #Nombre del usuario
@@ -36,10 +36,10 @@ class BaseDatosLvl1:
         #Establecemos la conexion con la base de datos atraves psycog2
         self.conn=psycopg2.connect(host=self.datahost,dbname=self.dataname, user=self.datauser, password=self.datapass,port=self.dataport)
         #Inicializamos el cursor con el que operaremos en la base de datos
-        self.cur=self.conn.cursor() 
+        self.cur=self.conn.cursor()
         #inicializamdos la conexopn que usara sqlAlchemy para operar en la base de datos
         conn_string = f'postgresql://{self.datauser}:{self.datapass}@{self.datahost}:{self.dataport}/{self.dataname}'
-        self.engine = create_engine(conn_string) 
+        self.engine = create_engine(conn_string)
         self.log=Log()
         self.log.limpiarLog()
         #llamamos a la función crear la cual creara las tablas que no esten creadas en la base de datos
@@ -47,9 +47,9 @@ class BaseDatosLvl1:
     ########################################################################################################################
         
     #Creamos las tablas de la base de datos la base de datos con los ultimos datos que hayamos obtenido
-    ##########################################################################################################################   
+    ##########################################################################################################################
     def crear(self):
-        #Creación de la tabla en caso de que no exista del skyscaner, 
+        #Creación de la tabla en caso de que no exista del skyscaner,
         #Sidedatehour tiene un formato de side,date(al estilo año-mes-día),horaini,horafin
         orden=""" CREATE TABLE IF NOT EXISTS skyscanner (side VARCHAR, hour time,date integer,sect1 decimal,sect2 decimal,
         sect3 decimal,sect4 decimal,sect5 decimal,sect6 decimal,sect7 decimal,sect8 decimal,sect9 decimal,sect10 decimal,sect11 decimal,sect12 decimal,
@@ -62,11 +62,12 @@ class BaseDatosLvl1:
         sect72 decimal,sect73 decimal,sect74 decimal,sect75 decimal,sect76 decimal,sect77 decimal,sect78 decimal,sect79 decimal,sect80 decimal,sect81 decimal,
         sect82 decimal,sect83 decimal,sect84 decimal,sect85 decimal,sect86 decimal,sect87 decimal,sect88 decimal,sect89 decimal,sect90 decimal,sect91 decimal,
         sect92 decimal,sect93 decimal,sect94 decimal,sect95 decimal,sect96 decimal,sect97 decimal,sect98 decimal,sect99 decimal,sect100 decimal,sect101 decimal,
-        sect102 decimal,sect103 decimal,sect104 decimal,sect105 decimal,sect106 decimal,sect107 decimal,sect108 decimal,sect109 decimal,sect110 decimal,sect111 decimal,
-        sect112 decimal,sect113 decimal,sect114 decimal,sect115 decimal,sect116 decimal,sect117 decimal,sect118 decimal,sect119 decimal,sect120 decimal,sect121 decimal,
-        sect122 decimal,sect123 decimal,sect124 decimal,sect125 decimal,sect126 decimal,sect127 decimal,sect128 decimal,sect129 decimal,sect130 decimal,sect131 decimal,
-        sect132 decimal,sect133 decimal,sect134 decimal,sect135 decimal,sect136 decimal,sect137 decimal,sect138 decimal,sect139 decimal,sect140 decimal,sect141 decimal,
-        sect142 decimal,sect143 decimal,sect144 decimal,sect145 decimal, azimut decimal, elevacion decimal,sidedatehour VARCHAR (255) PRIMARY KEY); """
+        sect102 decimal,sect103 decimal,sect104 decimal,sect105 decimal,sect106 decimal,sect107 decimal,sect108 decimal,sect109 decimal,sect110 decimal,
+        sect111 decimal,sect112 decimal,sect113 decimal,sect114 decimal,sect115 decimal,sect116 decimal,sect117 decimal,sect118 decimal,sect119 decimal,
+        sect120 decimal,sect121 decimal,sect122 decimal,sect123 decimal,sect124 decimal,sect125 decimal,sect126 decimal,sect127 decimal,sect128 decimal,
+        sect129 decimal,sect130 decimal,sect131 decimal,sect132 decimal,sect133 decimal,sect134 decimal,sect135 decimal,sect136 decimal,sect137 decimal,
+        sect138 decimal,sect139 decimal,sect140 decimal,sect141 decimal,sect142 decimal,sect143 decimal,sect144 decimal,sect145 decimal, azimut decimal,
+        elevacion decimal,sidedatehour VARCHAR (255) PRIMARY KEY); """
         #Enviamos la operación a la base de datos
         self.cur.execute(orden)
         self.conn.commit()
@@ -80,14 +81,16 @@ class BaseDatosLvl1:
         self.conn.commit()
         #Creación de las tablas en caso de que no exixta la radio
         #La primary key es timestamp la cual se divide en:año-mes-día hora
-        orden=""" CREATE TABLE IF NOT EXISTS radio ("TIMESTAMP" VARCHAR PRIMARY KEY,"Year" integer,"Month" integer,"Dia" integer,"YearDay" integer,"Hour" integer,"Minute" integer,
-        "BuPres_Avg" decimal,"BuRH_Avg" decimal,"BuTemp_Avg" decimal,"BuRain_Tot" decimal,"BuWS_Avg" decimal,"BuWD_Avg" decimal,"BuTDP_Avg" decimal,"BuTWB_Avg" decimal,
-        "BuRaGVN_Avg" decimal,"BuRaGVE_Avg" decimal,"BuRaGVS_Avg" decimal,"BuRaGVW_Avg" decimal,"BuRaGH_Avg" decimal,"BuRaDH_Avg" decimal,"BuRaB_Avg" decimal,"BuLxGVN_Avg" decimal,
-        "BuLxGVE_Avg" decimal,"BuLxGVS_Avg" decimal,"BuLxGVW_Avg" decimal,"BuLxGH_Avg" decimal,"BuLxDH_Avg" decimal,"BuLxB_Avg" decimal,"BuPaGVN_Avg" decimal,"BuPaGVE_Avg" decimal,
-        "BuPaGVS_Avg" decimal,"BuPaGVW_Avg" decimal,"BuPaGH_Avg" decimal,"BuPaDH_Avg" decimal,"BuPaB_Avg" decimal,"BuUvGVN_Avg" decimal,"BuUvGVE_Avg" decimal, "BuUvGVS_Avg" decimal,
-        "BuUvGVW_Avg" decimal,"BuUvGH_Avg" decimal,"BuUvDH_Avg" decimal,"BuUvB_Avg" decimal,"BuUvAGH_Avg" decimal,"BuUvADH_Avg" decimal,"BuUvAV_Avg" decimal,"BuUvBGH_Avg" decimal,
-        "BuUvBDH_Avg" decimal,"BuUvBV_Avg" decimal,"BuUvEGH_Avg" decimal,"BuUvEDH_Avg" decimal,"BuUvEV_Avg" decimal,"BuRaDVN_Avg" decimal,"BuRaDVE_Avg" decimal,"BuRaDVS_Avg" decimal,
-        "BuRaDVW_Avg" decimal,"BuRaAlUp_Avg" decimal,"BuRaAlDo_Avg" decimal,"BuRaAlbe_Avg" decimal,"BuPaR_Avg" decimal,"BuLxR_Avg" decimal,"BuIrGH_Avg" decimal,date integer)"""
+        orden=""" CREATE TABLE IF NOT EXISTS radio ("TIMESTAMP" VARCHAR PRIMARY KEY,"Year" integer,"Month" integer,"Dia" integer,"YearDay" integer,
+        "Hour" integer,"Minute" integer,"BuPres_Avg" decimal,"BuRH_Avg" decimal,"BuTemp_Avg" decimal,"BuRain_Tot" decimal,"BuWS_Avg" decimal,
+        "BuWD_Avg" decimal,"BuTDP_Avg" decimal,"BuTWB_Avg" decimal,"BuRaGVN_Avg" decimal,"BuRaGVE_Avg" decimal,"BuRaGVS_Avg" decimal,"BuRaGVW_Avg" decimal,
+        "BuRaGH_Avg" decimal,"BuRaDH_Avg" decimal,"BuRaB_Avg" decimal,"BuLxGVN_Avg" decimal,"BuLxGVE_Avg" decimal,"BuLxGVS_Avg" decimal,"BuLxGVW_Avg" decimal,
+        "BuLxGH_Avg" decimal,"BuLxDH_Avg" decimal,"BuLxB_Avg" decimal,"BuPaGVN_Avg" decimal,"BuPaGVE_Avg" decimal,"BuPaGVS_Avg" decimal,"BuPaGVW_Avg" decimal,
+        "BuPaGH_Avg" decimal,"BuPaDH_Avg" decimal,"BuPaB_Avg" decimal,"BuUvGVN_Avg" decimal,"BuUvGVE_Avg" decimal, "BuUvGVS_Avg" decimal,"BuUvGVW_Avg" decimal,
+        "BuUvGH_Avg" decimal,"BuUvDH_Avg" decimal,"BuUvB_Avg" decimal,"BuUvAGH_Avg" decimal,"BuUvADH_Avg" decimal,"BuUvAV_Avg" decimal,"BuUvBGH_Avg" decimal,
+        "BuUvBDH_Avg" decimal,"BuUvBV_Avg" decimal,"BuUvEGH_Avg" decimal,"BuUvEDH_Avg" decimal,"BuUvEV_Avg" decimal,"BuRaDVN_Avg" decimal,"BuRaDVE_Avg" decimal,
+        "BuRaDVS_Avg" decimal,"BuRaDVW_Avg" decimal,"BuRaAlUp_Avg" decimal,"BuRaAlDo_Avg" decimal,"BuRaAlbe_Avg" decimal,"BuPaR_Avg" decimal,"BuLxR_Avg" decimal,
+        "BuIrGH_Avg" decimal,date integer)"""
         #Enviamos la operación a la base de dactos
         self.cur.execute(orden)
         self.conn.commit()
@@ -96,30 +99,31 @@ class BaseDatosLvl1:
         #Enviamos la operación a la base de dactos
         self.cur.execute(orden)
         self.conn.commit()
-    ########################################################################################################################################################################################
+    #############################################################################################################################################################
         
-######################################################################################################################################################################################################
-#Zona de Obtención y descarga de datos 
+#################################################################################################################################################################
+#Zona de Obtención y descarga de datos
          
-    #Obtenemos los datos de una tabla especifica que se pasa por base a las columna que se pase por select y entre las fechas que se pasen atraves de cond1 y cond2
+    #Obtenemos los datos de una tabla especifica que se pasa por base a las columna que se pase por select y entre las fechas que se pasen 
+    #atraves de cond1 y cond2
     #Cond1 y cond2 tienes que pasarse con el estil año(sin el 20)-mes(de dos cifras siempre)-dia(de dos cifras siempre) y en string
     ####################################################################################################################
     def obtenerdat(self,selec,base,cond1,cond2):
-
-        
-        if cond1!=None and cond1!=None: #en caso de que conde no sea vacia 
+        if cond1 is not None and cond1 is not None: #en caso de que conde no sea vacia
             #usamos replace para eliminar los guiones y que sea igual que la fecha tipada
             cond1=cond1.replace('-', '')
             cond2=cond2.replace('-', '')
-            query="SELECT {cols} FROM {table} WHERE date BETWEEN {condic1} AND {condic2} ".format(cols=selec,table=base,condic1=cond1,condic2=cond2)
+            query="SELECT {} FROM {} WHERE date BETWEEN :condic1 AND :condic2".format(selec, base)
+            params = {"condic1": cond1, "condic2": cond2}
         else: #En caso de que no haya una condicion se toma todo
-            query="SELECT {cols} FROM {table}".format(cols=selec,table=base)
+            query="SELECT {} FROM {}".format(selec, base)
+            params = {}
+
         #Recogemos los datos en un data frame
         with self.engine.connect() as db_conn:
-            data = pd.read_sql(sql=text(query),con=db_conn)
+            data = pd.read_sql(sql=text(query), params=params, con=db_conn)
             df=pd.DataFrame(data)
         #Devolvemos los datos que se encuentran en esa tabla
-        return df
     ######################################################################################################################
 
     #Descargamos los datos de una tabla especifica que se pasa por base a las columna que se pase por select y entre las fechas que se pasen atraves de cond1 y cond2
