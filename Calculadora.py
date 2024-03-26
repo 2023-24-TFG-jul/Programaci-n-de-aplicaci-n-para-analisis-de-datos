a61788871e47a0b6f9d29020e0a2dc50455de1a4
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-03-26 22:01:14 +0100

    Calc Ver:0.0.1 Inicialización de la calculadora

---
+++ Calculadora.py
@@ -0,0 +1,9 @@
+#Nombre:Calculadora
+#Autor:Álvaro Villar Val
+#Fecha:26/03/24
+#Versión:0.0.1
+#Descripción: Calculadora de los diferentes criterios de calidad de la central meteorologica
+#########################################################################################################################
+#Definimos los imports
+from pysolar.solar import *
+import datetime
