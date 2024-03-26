993dc558519ed2f08bb2df32d7a3fee189482bc3
AlvaroVillarVal <alvafrankvvmm@gmail.com>
2024-03-26 22:42:48 +0100

    Calc Ver: 0.0.2 añadidos los metodos que tendremos que rellenar en la calculadora con los datos necesarios

--- Calculadora.py
+++ Calculadora.py
@@ -1,9 +1,91 @@
 #Nombre:Calculadora
 #Autor:Álvaro Villar Val
 #Fecha:26/03/24
-#Versión:0.0.1
+#Versión:0.0.2
 #Descripción: Calculadora de los diferentes criterios de calidad de la central meteorologica
 #########################################################################################################################
 #Definimos los imports
 from pysolar.solar import *
 import datetime
+
+class Calculadora:
+    def ghiPhys(self,latitud,longitud,fecha):
+        return 0
+    def ghiSky(self,latitud,longitud,fecha):
+        return 0
+    def ghiCohe(self,latitud,longitud,fecha):
+        return 0
+    def dhiPhys(self,latitud,longitud,fecha):
+        return 0
+    def dhiSky(self,latitud,longitud,fecha):
+        return 0
+    def dhiCohe(self,latitud,longitud,fecha):
+        return 0
+    def dniPhys(self,latitud,longitud,fecha):
+        return 0
+    def dniSky(self,latitud,longitud,fecha):
+        return 0
+    def dniCohe(self,latitud,longitud,fecha):
+        return 0
+    
+
+
+    def ghilPhys(self,latitud,longitud,fecha):
+        return 0
+    def ghilSky(self,latitud,longitud,fecha):
+        return 0
+    def ghilCohe(self,latitud,longitud,fecha):
+        return 0
+    def dhilPhys(self,latitud,longitud,fecha):
+        return 0
+    def dhilSky(self,latitud,longitud,fecha):
+        return 0
+    def dhilCohe(self,latitud,longitud,fecha):
+        return 0
+    def dnilPhys(self,latitud,longitud,fecha):
+        return 0
+    def dnilSky(self,latitud,longitud,fecha):
+        return 0
+    def dnilCohe(self,latitud,longitud,fecha):
+        return 0
+    
+
+    def ghpPhys(self,latitud,longitud,fecha):
+        return 0
+    def ghpSky(self,latitud,longitud,fecha):
+        return 0
+    def ghpCohe(self,latitud,longitud,fecha):
+        return 0
+    def dhpPhys(self,latitud,longitud,fecha):
+        return 0
+    def dhpSky(self,latitud,longitud,fecha):
+        return 0
+    def dhpCohe(self,latitud,longitud,fecha):
+        return 0
+    def dnpPhys(self,latitud,longitud,fecha):
+        return 0
+    def dnpSky(self,latitud,longitud,fecha):
+        return 0
+    def dnpCohe(self,latitud,longitud,fecha):
+        return 0
+  
+
+    def ghuvPhys(self,latitud,longitud,fecha):
+        return 0
+    def ghuvSky(self,latitud,longitud,fecha):
+        return 0
+    def ghuvCohe(self,latitud,longitud,fecha):
+        return 0
+    def dhuvPhys(self,latitud,longitud,fecha):
+        return 0
+    def dhuvSky(self,latitud,longitud,fecha):
+        return 0
+    def dhuvCohe(self,latitud,longitud,fecha):
+        return 0
+    def dnuvPhys(self,latitud,longitud,fecha):
+        return 0
+    def dnuvSky(self,latitud,longitud,fecha):
+        return 0
+    def dnuvCohe(self,latitud,longitud,fecha):
+        return 0
+    
