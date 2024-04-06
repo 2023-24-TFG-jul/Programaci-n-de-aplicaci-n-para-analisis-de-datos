80293eb0001ff047615a8d5f194042558adf2c0f
Álvaro <alvafrankvvmm@gmail.com>
2024-04-07 00:01:36 +0200

    Calc Ver: 0.2.1 Optimización de las funciones de calculo fisico para reducir el codigo repetido

--- Calculadora.py
+++ Calculadora.py
@@ -1,7 +1,7 @@
 #Nombre:Calculadora
 #Autor:Álvaro Villar Val
 #Fecha:26/03/24
-#Versión:0.2.0
+#Versión:0.2.1
 #Descripción: Calculadora de los diferentes criterios de calidad de la central meteorologica
 #########################################################################################################################
 #Definimos los imports
@@ -29,6 +29,29 @@ class Calculadora:
         minuto=int(str(fecha)[14:16])
         return datetime.datetime(año, mes, dia, hora, minuto, tzinfo=datetime.timezone.utc)
     
+    def physGen1(self,value,altitude,numFin,dn0,numIni,min):
+        max=dn0*numIni*(math.cos(altitude)**1.2)+numFin
+        if value>-min and value<=max:
+             return 1
+        elif value>min:
+            return 2
+        else:
+            return 3
+        
+    def physGen2(self,value,dn0,min):
+        if value>min:
+            if value<=dn0:
+                return 1
+            else:
+                return 3
+        else:
+            return 2
+    #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
+    def ghiSky(self,value):
+        if value<=self.ghiclear:
+             return True
+        else:
+            return False
     def comprobar (self,valuePrin,funPhys,funSky,cohe1,cohe2,cohe3,cohe4,valueGH,valueDH,valueDN,fecha):
         date = self.dates(fecha)
         grado=get_altitude(self.latitude, self.longitude, date)
@@ -67,14 +90,8 @@ class Calculadora:
             
     
     def ghiPhys(self,value,altitude):
-       
-        max=self.dni0*1.5*(altitude**1.2)+100
-        if value>-4 and value<=max:
-             return 1
-        elif value>-4:
-            return 2
-        else:
-            return 3
+       return self.physGen1(value,altitude,100,self.dni0,1.5,-4)
+    
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def ghiSky(self,value):
         if value<=self.ghiclear:
@@ -88,14 +105,8 @@ class Calculadora:
     
     #Physical limits
     def dhiPhys(self,value,grado):
-        max=self.dni0*0.95*(math.cos(grado)**1.2)+50
-        if value>-4:
-            if value<=max:
-             return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen1(value,grado,50,self.dni0,0.95,-4)
+    
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def dhiSky(self,value):
         if value<=self.ghiclear:
@@ -109,13 +120,7 @@ class Calculadora:
     
     #Physical limits
     def dniPhys(self,value):
-        if value>-4:
-            if value<=self.dni0:
-                return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen2(value,self.dni0,-4)
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def dniSky(self,value):
         if value<=self.dniclear:
@@ -165,14 +170,8 @@ class Calculadora:
     
     #Physical limits
     def ghilPhys(self,value,grado):
-        max=self.dnil0*1.5*(math.cos(grado)**1.2)+10000
-        if value>0: 
-            if value<=max:
-             return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen1(value,grado,10000,self.dnil0,1.5,0)
+    
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def ghilSky(self,value):
         return True
@@ -183,14 +182,8 @@ class Calculadora:
     
     #Physical limits
     def dhilPhys(self,value,grado):
-        max=self.dnil0*0.95*(math.cos(grado)**1.2)+5000
-        if value>0:
-            if value<=max:
-             return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen1(value,grado,5000,self.dnil0,0.95,0)
+        
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def dhilSky(self,value):
         return True
@@ -201,13 +194,7 @@ class Calculadora:
     
     #Physical limits
     def dnilPhys(self,value):
-        if value>-0:
-            if value<=self.dnil0:
-                return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen2(value,self.dnil0,0)
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def dnilSky(self,value):
         return 0
@@ -256,13 +243,8 @@ class Calculadora:
         return self.comprobar(valueGhp,self.ghpPhys,self.ghpSky,self.coheP1,self.coheP2,self.coheP3,self.coheP4,valueGhp,valueDHp,valueDNp,fecha)
             
     #Physical limits
-    def ghpPhys(self,value,fecha):
-        date = self.dates(fecha)
-        max=self.dnp0*1.5*(math.cos(get_altitude(self.latitude, self.longitude, date))**1.2)+40
-        if value>-0 and value<=max:
-             return True
-        else:
-            return False
+    def ghpPhys(self,value,grado):
+        return self.physGen1(value,grado,40,self.dnp0,1.5,0)
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def ghpSky(self,value):
         max=(46.5325*self.m**2-1738.11*self.m+48907.2)/(4.78443*self.m**2+89.17*self.m+1)
@@ -277,14 +259,8 @@ class Calculadora:
     
     #Physical limits
     def dhpPhys(self,value,grado):
-        max=self.dnp0*0.95*(math.cos(grado)**1.2)+20
-        if value>-0:
-            if value<=max:
-                return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen1(value,grado,20,self.dnp0,0.95,0)
+        
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def dhpSky(self,value):
         max=(-0.489631*self.m**2+17.4211*self.m+51.858)/(0.0575636*self.m**2+0.671139*self.m+1)
@@ -299,13 +275,8 @@ class Calculadora:
     
     #Physical limits
     def dnpPhys(self,value):
-        if value>-0:
-            if value<=self.dnp0:
-                return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen2(value,self.dnp0,0)
+        
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def dnpSky(self,value):
         max=(0.171991*self.m**2-9.88174*self.m+532.694)/(0.00732718*self.m**2+0.13576*self.m+1)
@@ -360,14 +331,8 @@ class Calculadora:
     
     #Physical limits
     def ghuvPhys(self,value,grado):
-        max=self.dnuv0*1.5*(math.cos(grado)**1.2)+5
-        if value>-0:
-            if value<=max:
-             return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen1(value,grado,5,self.dnuv0,1.5,0)
+        
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def ghuvSky(self,value):
         max=(1.78513*self.m^2+177.076*self.m+2594.06)/(13.8072*self.m**2+25.6894*self.m+1)
@@ -382,14 +347,8 @@ class Calculadora:
     
     #Physical limits
     def dhuvPhys(self,value,grado):
-        max=self.dnuv0*0.95*(math.cos(grado)**1.2)+2
-        if value>-0:
-            if value<=max:
-             return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen1(value,grado,2,self.dnuv0,0.95,0)
+        
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def dhuvSky(self,value):
         max=(0.0284353*self.m**2-0.773392*self.m+34.2974)/(0.0393782*self.m**2+0.593745*self.m+1)
@@ -405,13 +364,7 @@ class Calculadora:
     
     #Physical limits
     def dnuvPhys(self,value):
-        if value>-0:
-            if value<=self.dnuv0:
-                return 1
-            else:
-                return 3
-        else:
-            return 2
+        return self.physGen2(value,self.dnuv0,0)
     #Limits of a clean and dry clear sky condition (without water vapor and aerosols)
     def dnuvSky(self,value):
         max=(0.613588*self.m**2-14.0356*self.m+88.664)/(0.0966512*self.m**2+0.474748*self.m+1)
