ff20467e6faa42f95f23b6500ab97940ac1a6ad5
Álvaro <alvafrankvvmm@gmail.com>
2024-04-12 21:45:21 +0200

    BD1_test Ver:0.3.0 Añadida la compobación de buen funcionamiento de descarga de datos e imagenes y por ello completado el Unitest de BD1 Closes #64;

--- BD1_test.py
+++ BD1_test.py
@@ -1,13 +1,13 @@
 #Nombre:BD1_test
 #Autor:Álvaro Villar Val
 #Fecha:07/03/24
-#Versión:0.2.1
+#Versión:0.3.0
 #Descripción: Test para de la creación de la base de datos
 #########################################################################################################################
 #Definimos los imports
 import unittest #Importamos la librería unittest
 from BaseDatosLvl1 import BaseDatosLvl1 #Importamos la clase BaseDatosLvl1
-
+import os #Importamos la librería os
 
 class TestBaseDatosLvl1(unittest.TestCase):
 
@@ -23,6 +23,15 @@ class TestBaseDatosLvl1(unittest.TestCase):
         self.db.cur.execute("DROP TABLE IF EXISTS imagescam1")
         self.db.conn.commit()
         self.db.conn.close()
+        datos=os.listdir("DatosResulta")
+        if len(datos)>0:
+            for i in datos:
+                os.remove("DatosResulta/"+i)
+        img=os.listdir("FotosResulta")
+        if len(img)>0:
+            for i in img:
+                os.remove("FotosResulta/"+i)
+        self.db.stop()
 
     def test_table_skyscanner_exists(self):
         # Comprobamos que se crea la tabla skyscanner
@@ -46,7 +55,7 @@ class TestBaseDatosLvl1(unittest.TestCase):
         result = self.db.actuimgCam1()
         self.assertTrue(result==0)  # Ponemos que sea 0 el resultado´
 
-    def test_actualizardatos_y_obtenerdat(self):
+    def test_actualizardatos_obtenerda_y_descDat(self):
         # Comprobamos que en una base de datos nueva no hay errores al actualizar los datos
         # La función devuelve el nº de errores que se han producido al actualizar los datos
         #Por lo cual en una nueva base de datos el resultado debería ser 0
@@ -56,6 +65,18 @@ class TestBaseDatosLvl1(unittest.TestCase):
         self.assertTrue(err3==0)
         result=self.db.obtenerdat("*","radio","23-10-01","23-10-01")
         self.assertFalse(result.empty)
+        self.db.descDat("*","radio","23-10-01","23-10-01")
+        datos=os.listdir("DatosResulta")
+        self.assertTrue(len(datos)>0)
+
+    def test_descImg(self):
+        # Comprobamos que en una base de datos nueva no hay errores al descargar las imagenes
+        # La función devuelve el nº de errores que se han producido al descargar las imagenes
+        #Por lo cual en una nueva base de datos el resultado debería ser 0
+        self.db.actuimgCam1()
+        self.db.obtenerImg("23-11-29-10","23-11-29-10")
+        img=os.listdir("FotosResulta")
+        self.assertTrue(len(img)>0)
 
 
 if __name__ == '__main__':
